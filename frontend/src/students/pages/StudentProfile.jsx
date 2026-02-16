c8e2b8d2c7c5a768108f194de6d7cbcf581df3d4
IronLeaf <146615281+nem-web@users.noreply.github.com>
2026-02-16 18:11:04 +0530

    Remove unused import of useRef in StudentProfile

--- frontend/src/students/pages/StudentProfile.jsx
+++ frontend/src/students/pages/StudentProfile.jsx
@@ -19,7 +19,6 @@ import {
 import StudentNavigation from "../components/StudentNavigation"
 import { Link } from "react-router-dom";
 import { uploadFaceImage } from "../../api/students"
-import { useRef } from "react";
 import { useTranslation } from "react-i18next";
 
 export default function StudentProfile() {
